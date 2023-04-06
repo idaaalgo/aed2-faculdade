3fa2b58fd72cbeaba8ef568caaf597e5f9cd1fe5
Eduardo Coutinho Idalgo <104573566+idaaalgo@users.noreply.github.com>
2023-04-05 22:01:04 -0300

    Create exercicio1.c

---
+++ lista-modularizacao/exercicio1.c
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Eduardo Idalgo
+Lista Modularização
+1. Escreva um programa que implemente uma função que retorne a diferença entre dois
+números inteiros digitados pelo usuário.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int retornaDiferenca(int a, int b) {
+    if(a>b){
+        return a-b;    
+    }
+    return b-a;
+}
+
+void main() {
+    
+    int a, b;
+    
+    printf("Entre com o valor do primeiro número: ");
+    scanf("%d", &a);
+    printf("Entre com o valor do segundo número: ");
+    scanf("%d", &b);
+    
+    int diferenca = retornaDiferenca(a, b);
+    printf("A diferença de %d e %d é %d", a, b, diferenca);
+    
+}
