591130968e93ea9d5f3ac411c512858b59e4406f
Eduardo Coutinho Idalgo <104573566+idaaalgo@users.noreply.github.com>
2023-04-06 10:01:47 -0300

    Create exercicio2.c

---
+++ lista-modularizacao/exercicio2.c
@@ -0,0 +1,42 @@
+/******************************************************************************
+
+Eduardo Idalgo
+Lista Modularização
+2. Escreva uma função que retorne a divisão entre dois números inteiros.
+Atenção para a questão da divisão por zero!
+A divisão não pode ocorrer se o divisor for zero
+
+*******************************************************************************/
+#include <stdio.h>
+
+
+/*A função retornaDivisao recebe dividendo e divisor
+e retorna o resultado de dividendo/divisor.
+Caso o valor do divisor seja diferente de 0
+a função ocorre normalmente, caso contrário
+a função retorna 0*/
+float retornaDivisao(int dividendo, int divisor) {
+    if(divisor==0){
+        printf("Valor de divisor 0 é inválido");
+        return 0;
+    }
+    return (float)dividendo/divisor;
+}
+
+void main() {
+    
+    int dividendo, divisor;
+   //Entrada de dados.
+    printf("Entre com o valor do dividendo: ");
+    scanf("%d", &dividendo);
+    printf("Entre com o valor do divisor diferente de 0: ");
+    scanf("%d", &divisor);
+   /*A variável resultado chama a função retornaDivisao
+   e guarda o resultado para si*/
+    float resultado = retornaDivisao(dividendo, divisor);
+//Saída
+  //Imprime somente a mensagem de resultado caso o valor de divisor !=0
+    if(divisor != 0) {
+        printf("A divisão de %d por %d é %.3f", dividendo, divisor, resultado);
+    }
+}
