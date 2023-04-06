8ba3effe8175530040042e35990758a940416f95
Eduardo Coutinho Idalgo <104573566+idaaalgo@users.noreply.github.com>
2023-04-06 09:39:11 -0300

    Update exercicio1.c

--- lista-modularizacao/exercicio1.c
+++ lista-modularizacao/exercicio1.c
@@ -9,20 +9,25 @@ números inteiros digitados pelo usuário.
 *******************************************************************************/
 #include <stdio.h>
 
+
+/*Função retornaDiferenca, receberá dois pârametros(a, b) e irá retornar
+a subtração de a e b*/
 int retornaDiferenca(int a, int b) {
     return a-b;
 }
 
 void main() {
     
-    int a, b;
-    
+    int a, b, diferenca;
+//Entrada de dados
     printf("Entre com o valor do primeiro número: ");
     scanf("%d", &a);
     printf("Entre com o valor do segundo número: ");
     scanf("%d", &b);
-    
-    int diferenca = retornaDiferenca(a, b);
+/*A variável diferenca recebera o retorno da função
+retornaDiferenca*/
+    diferenca = retornaDiferenca(a, b);
+//Saída do programa
     printf("A diferença de %d e %d é %d", a, b, diferenca);
     
 }
