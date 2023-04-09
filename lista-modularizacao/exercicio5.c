8c5e5b7e7ab98c2d8e4a65fb5231b42b49ea2279
Eduardo Coutinho Idalgo <104573566+idaaalgo@users.noreply.github.com>
2023-04-09 20:23:09 -0300

    Update exercicio5.c

--- lista-modularizacao/exercicio5.c
+++ lista-modularizacao/exercicio5.c
@@ -8,6 +8,8 @@ Dica: Use a tabela ASCII.
 *******************************************************************************/
 #include <stdio.h>
 
+//Essa função recebe um caracter,
+//e verificae se ele está entre a/A e z/Z
 int entre_a_e_z(char c) {
     if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') {
         return 1;
@@ -15,14 +17,19 @@ int entre_a_e_z(char c) {
     return 0;
 }
 
-int main() {
+void main() {
     char c;
+    //Entrada de dados
     printf("Digite um caracter: \n");
     scanf("%c", &c);
+    //Lógica do programa
+    
+    //Se ele estiver dentro de a e z usando a função,
+    //imprime na tela "%c Está entre 'a/A' e 'z/Z'"
     if (entre_a_e_z(c)) {
         printf("%c Está entre 'a/A' e 'z/Z'\n", c);
-    } else {
+    } else {//Caso contrário, imprime "%c Não está entre 'a/A' e 'z/Z'" 
         printf("%c Não está entre 'a/A' e 'z/Z'\n", c);
     }
-    return 0;
+    
 }
